d9035f3f4ffea040cc566c4a7e71d2812740df9d
codecrook <hello@codecrook.io>
2018-10-04 13:47:08 +0530

    Created the MVC structure skeleton for the app
    What's Done:
    
    - Created MVC organizational pattern to execute separation of concern
    - Added EventListeners to listen click event on Add Button and keypress event on document

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 {
-    
+    'use strict';
+    let budgetModel = (function () {
+        
+    })();
+
+    let budgetView = (function () {
+        
+    })();
+
+    let controller = (function (budMod, budVw) {
+        let addButton = document.querySelector('.add__btn');
+        
+        addButton.addEventListener('click', (e) => {
+            //1. Get the field input data
+            alert('clicked!!');
+            //2. Add the data to budgetModel
+
+            //3. Add the item to the UI
+
+            //4. Calculate the budget
+
+            //5. Display the budget on the UI
+
+
+        });
+
+        document.addEventListener('keypress', (e) => {
+            if (e.keyCode === 13 || e.which === 13) addButton.click(); 
+        });
+
+    })(budgetModel, budgetView);
 }
