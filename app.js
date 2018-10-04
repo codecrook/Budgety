86616b1c7c65129d3d8804beeae49f6556b686ec
codecrook <hello@codecrook.io>
2018-10-04 15:41:46 +0530

    Added functionalities to get input data from the input field
    What's Done:
    
    - Created a CONST to store all the classnames of the Inut element
    - Added functions to return input data to Controller from the View
    - Everything's done in MVC pattern

--- app.js
+++ app.js
@@ -1,30 +1,57 @@
 {
     'use strict';
-    let budgetModel = (function () {
+
+    //MODEL
+    let budgetModel = (() => {
         
     })();
 
-    let budgetView = (function () {
-        
+
+    //VIEW
+    let budgetView = (() => {
+        const DOMStrings = {
+            inputType: '.add__type',
+            inputDecription: '.add__description',
+            inputValue: '.add__value',
+            inputBtn: '.add__btn'
+        };
+
+        return {
+            getInput() {
+                return {
+                    type: document.querySelector(DOMStrings.inputType).value,
+                    description: document.querySelector(DOMStrings.inputDecription).value,
+                    value: document.querySelector(DOMStrings.inputValue).value
+                };
+            },
+            getDOMStrings() {
+                return DOMStrings;
+            }
+        };
     })();
 
-    let controller = (function (budMod, budVw) {
-        let addButton = document.querySelector('.add__btn');
-        
-        addButton.addEventListener('click', (e) => {
+
+    //CONTROLLER
+    let controller = ((budMod, budVw) => {
+
+        const controllerDOMStrings = budVw.getDOMStrings();
+        const addButton = document.querySelector(controllerDOMStrings.inputBtn);
+
+
+        let ctrlAddItem = () => {
             //1. Get the field input data
-            alert('clicked!!');
+            const input = budVw.getInput();
+            console.log(input);
             //2. Add the data to budgetModel
-
+ 
             //3. Add the item to the UI
-
+ 
             //4. Calculate the budget
-
+ 
             //5. Display the budget on the UI
-
-
-        });
-
+        };
+        
+        addButton.addEventListener('click', ctrlAddItem);
         document.addEventListener('keypress', (e) => {
             if (e.keyCode === 13 || e.which === 13) addButton.click(); 
         });
